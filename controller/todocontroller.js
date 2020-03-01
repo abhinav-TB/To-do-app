51d42c0ab3d62f8ca384e200053944e44327f03d
abhinav-TB <abhinavtb@gmail.com>
2020-03-01 09:33:03 +0530

    initial coomits

--- controller/todocontroller.js
+++ controller/todocontroller.js
@@ -1,23 +1,43 @@
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose') 
+//connect to databse
+mongoose.connect('mongodb+srv://abhinav:abhinav123@cluster0-fbvaq.mongodb.net/test?retryWrites=true&w=majority',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    });
 
-var data=[{item:'get milk'},{item:'walk dog'},{item:'kick some coding ass'}];
+//vreate a schema
+var todoschema =new mongoose.Schema({
+    item: String
+});
+
+var todo =mongoose.model('todo',todoschema);
+
+
+// var data=[{item:'get milk'},{item:'walk dog'},{item:'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 module.exports =function(app){
 app.get('/todo',function(req,res){
-    res.render('todo',{todos:data}); 
-console.log("hey");
+    todo.find({},function(err,data){
+        if(err) throw err;
+        res.render('todo',{todos:data}); 
+    });
+    
+
 });
 app.post('/todo', urlencodedParser,function(req,res){
-    console.log("hello");
-    data.push(req.body);
+    var newtodo=todo(req.body).save(function(err,data){
+        if(err) throw err;
+        res.json(data);
+    })
     
-    res.json(data);
+    
+   
 });
 app.delete('/todo/:item',function(req,res){
-    data=data.filter(function(todo){
-        return todo.item.replace(/ /g, '-')!=req.paams.item;
-    });
-    res.json({todo: data});
+todo.find({item: req.params.item.replace(/\-/g," ")}).deleteOne(function(err,data){
+    if(err) throw err;
+});
 
 });
 };
